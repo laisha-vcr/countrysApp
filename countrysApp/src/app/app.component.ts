a042ec61be190caa0c8c59a281541378320ab644
laisha-vcr <53242338+laisha-vcr@users.noreply.github.com>
2025-02-24 11:43:39 -0600

    nuevo
    nuevos cambios <.>

--- countrysApp/src/app/app.component.ts
+++ countrysApp/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SharedModule } from "./shared/shared.module";
-import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [SharedModule, RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
